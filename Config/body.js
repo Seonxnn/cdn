e5df7a272cecb37db25222fafa1c5f710a0742e6
Seonxnn <96802119+Seonxnn@users.noreply.github.com>
2024-09-24 13:49:34 +0700

    Create body.js

---
+++ Config/body.js
@@ -0,0 +1,6 @@
+<script>/*<![CDATA[*/
+const ThemeManagerConfig = {
+   autoThemerInterval: 5000, // Time interval for auto themer in milliseconds
+   defaultToggleState: true, // Default toggle state for auto theme changer when the user visits the site for the first time
+};
+/*]]>*/</script>
